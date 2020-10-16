34680813146a28a45a137f8e639d36c4dfbc3f19
pavel1337228 <64475489+pavel1337228@users.noreply.github.com>
2020-10-17 00:52:52 +0300

    pp

--- Assets/Scripts/DotMove.cs
+++ Assets/Scripts/DotMove.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class DotMove : MonoBehaviour
 {
+
+    private RectTransform Char;
+    private GameObject[] massive;
+    private int i;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Char = GetComponent<RectTransform>();
+        massive = GameObject.FindGameObjectsWithTag("Dot");
+        i = Random.Range(0, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Char.transform.position != massive[i].GetComponent<RectTransform>().transform.position)
+        {
+            Char.transform.position = Vector3.MoveTowards(Char.transform.position, massive[i].GetComponent<RectTransform>().transform.position, Time.deltaTime);
+        }
+        if (Char.transform.position == massive[i].GetComponent<RectTransform>().transform.position)
+        {
+            i = Random.Range(0, 3);
+        }
+        //Char.anchoredPosition = Mathf.Lerp(Char.anchoredPosition, massive[2].GetComponent<RectTransform>(), Time.deltaTime*2);
+        //Char.anchoredPosition = new Vector2(massive[2].GetComponent<RectTransform>().anchoredPosition, massive[2].GetComponent<RectTransform>().anchoredPosition);
     }
 }
